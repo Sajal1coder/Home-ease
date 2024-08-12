17675ebdc71fec9d6038bd162202ac214fcec57f
Sajal1coder <157198364+Sajal1coder@users.noreply.github.com>
2024-08-12 10:27:00 +0530

    ALL PROBLEM are sorted out for upto navbar.

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
-import store from './store'; // Import your Redux store
+import { store, persistor } from "./redux/store"; // Import your Redux store
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
