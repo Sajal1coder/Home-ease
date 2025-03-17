719a14cff43974b15bae3e272258f75d5e655121
Sajal1coder <157198364+Sajal1coder@users.noreply.github.com>
2025-03-17 12:33:46 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const listingRoutes = require("./routes/listing.js")
 const BookingRoutes=require("./routes/booking.js")
 const userRoutes=require("./routes/user.js")
 
-app.use(cors());
+app.use(cors("https://homease-taupe.vercel.app/"));
 app.use(express.json());
 app.use(express.static("public"));
 
