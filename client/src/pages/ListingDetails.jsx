d2e9971a1a4550af9f03dd6ac95e0765bed42a8f
Sajal1coder <157198364+Sajal1coder@users.noreply.github.com>
2025-03-19 10:41:43 +0530

    final

--- client/src/pages/ListingDetails.jsx
+++ client/src/pages/ListingDetails.jsx
@@ -96,7 +96,7 @@ const ListingDetails = () => {
         token,
       };
 
-      const response = await fetch("http://localhost:3001/bookings/create", {
+      const response = await fetch("https://home-easy-green.vercel.app/bookings/create", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -127,7 +127,7 @@ const ListingDetails = () => {
         <div className="photos">
           {listing.listingPhotoPaths?.map((item) => (
             <img
-              src={`http://localhost:3001/${item.replace("public", "")}`}
+              src={`https://home-easy-green.vercel.app/${item.replace("public", "")}`}
               alt="listing photo"
             />
           ))}
@@ -145,7 +145,7 @@ const ListingDetails = () => {
 
         <div className="profile">
           <img
-            src={`http://localhost:3001/${listing.creator.profileImagePath.replace(
+            src={`https://home-easy-green.vercel.app/${listing.creator.profileImagePath.replace(
               "public",
               ""
             )}`}
