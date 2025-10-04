85b2dca7b76e5893a6be31ce0fa16d3e22b8b223
sajal kumar baranwal <sajalkumarbaranwal13@gmail.com>
2025-10-04 19:59:46 +0530

    Chande for prod

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 // API Configuration
-export const API_BASE_URL = 'http://localhost:5000';
+//export const API_BASE_URL = 'http://localhost:5000';
 
-//export const API_BASE_URL = 'https://home-ease-backend.onrender.com';
+export const API_BASE_URL = 'https://home-ease-backend.onrender.com';
 
 export default API_BASE_URL;
